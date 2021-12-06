910fb1f84ffef0252417d421b31f4043158ee155
mtzcorporations <matjaz.zupanic1@gmail.com>
2021-12-06 15:51:23 +0100

    Popravki oblike kode

--- src/audio.py
+++ src/audio.py
@@ -1,75 +1,89 @@
 import struct
 
-import numpy as np
 import pyaudio
-import wave
-import sys
+from scipy import optimize
 
-from pyqtgraph.Qt import QtGui
-from scipy.fft import fft
+import numpy as np
 
+def exponentFun(x, x0, y0, k):
+    return -k*(x-x0)**2+y0
 
-class AudioFile:
-    chunk = 1024
+def findPeak(f,Pxx):
+    i_max = Pxx.argmax(axis=0)
+    if i_max > 5:
+        try:
+            fR = f[i_max - 1:i_max + 2]
+            PR = Pxx[i_max - 1:i_max + 2]
+            popt, _ = optimize.curve_fit(exponentFun, fR, PR)  # začetna funkcija, x,y
+            x2 = np.linspace(fR[0], fR[-1], 50)
+            y2 = exponentFun(x2, *popt)
 
-    def __init__(self,curve):
-        # stream constants
-        self.curve=curve
-        self.FORMAT = pyaudio.paInt16
-        self.CHANNELS = 1
-        self.RATE = 44100
-        self.pause = False
+            M = y2.argmax(axis=0)
 
-        # stream object
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format=self.FORMAT,
-            channels=self.CHANNELS,
-            rate=self.RATE,
-            input=True,
-            output=True,
-            frames_per_buffer=self.chunk,
-        )
+            return x2[M],x2,y2
+        except Exception:
+            pass
+    return None
 
-    def play(self):
-        """ Play entire file """
+FORMAT = pyaudio.paInt16
+RATE = 44100
+CHANNELS = 2
 
-        while True:
-            data = self.stream.read(self.chunk)
-            y = self.getSpecter(data)
-            ##self.curve.setData(y)
+# Set Plot Range [-RANGE,RANGE], default is nyquist/2
+RANGE = RATE/2
 
-            #self.curve.setPos()
+# Set these parameters (How much data to plot per FFT)
+INPUT_BLOCK_TIME = 0.05
+INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
 
-    def close(self):
-        """ Graceful shutdown """
-        self.stream.close()
-        self.p.terminate()
+# Which Channel? (L or R)
+LR = "l"
+class Audio():
+    def __init__(self):
+        self.pa = pyaudio.PyAudio()
+        self.initMicrophone()
 
-    def getSpecter(self, data):
+    def initMicrophone(self):
+        device_index = self.find_input_device()
 
-        # data_int = struct.unpack(str(2 * self.chunk) + 'B', data)
-        # data_np = np.array(data_int, dtype='b')[::2] + 128
-        # # compute FFT and update line
-        # yf = fft(data_int)
-        # y = np.abs(yf[0:self.chunk]) / (128 * self.chunk)
+        self.stream = self.pa.open(format=FORMAT,
+                                   channels=CHANNELS,
+                                   rate=RATE,
+                                   input=True,
+                                   input_device_index=device_index,
+                                   frames_per_buffer=INPUT_FRAMES_PER_BLOCK)
+    def find_input_device(self):
+        device_index = None
+        for i in range(self.pa.get_device_count()):
+            devinfo = self.pa.get_device_info_by_index(i)
+            if devinfo["name"].lower() in ["mic","input"]:
+                device_index = i
 
-        data = ((data / np.power(2.0, 15)) * 5.25)
+        return device_index
+    def readData(self):
+        block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
+        count = len(block)/2
+        format = "%dh"%(count)
+        shorts = struct.unpack( format, block )
+        if CHANNELS == 1:
+            return np.array(shorts)
+        else:
+            l = shorts[::2]
+            r = shorts[1::2]
+            if LR == 'l':
+                return np.array(l)
+            else:
+                return np.array(r)
 
-        # compute FFT parameters
-        f_vec = self.RATE * np.arange(self.chunk / 2) / self.chunk  # frequency vector based on window size and sample rate
-        mic_low_freq = 100
-        low_freq_loc = np.argmin(np.abs(f_vec - mic_low_freq))
-        fft_data = (np.abs(np.fft.fft(data))[0: int(np.floor(self.chunk / 2))]) / self.chunk
-        fft_data[1:] = 2 * fft_data[1:]
+    def get_spectrum(self, data):
+        T = 1.0 / RATE
+        N = data.shape[0]
+        Pxx = (1. / N) * np.fft.fft(data)
+        f = np.fft.fftfreq(N, T)
+        Pxx = np.fft.fftshift(Pxx)
+        f = np.fft.fftshift(f)
 
-        max_loc = np.argmax(fft_data[low_freq_loc:]) + low_freq_loc
-        print(max_loc)
-        return max_loc
-#
-#     # Usage example for pyaudio
-# a = AudioFile()
-# a.play()
-# a.close()
-#
-# p.terminate()
+        Pxx = Pxx[len(Pxx) // 2:]
+        f = f[len(f) // 2:]
+
+        return f, (np.absolute(Pxx))
--- src/test.py
+++ src/test.py
@@ -1,86 +1,18 @@
-#!/usr/bin/python
-
-"""
-PyAudio + PyQtGraph Spectrum Analyzer
-
-Author:@sbarratt
-Date Created: August 8, 2015
-
-"""
-
-import pyaudio
-import struct
-import math
-
-from astropy import modeling
-from matplotlib import pyplot as plt
-from pyqtgraph import mkPen
-from scipy import optimize
 import sys
-import numpy as np
 
 import pyqtgraph as pg
-from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph import mkPen
+from pyqtgraph.Qt import QtGui
 
 # Audio Format (check Audio MIDI Setup if on Mac)
-FORMAT = pyaudio.paInt16
-RATE = 44100
-CHANNELS = 2
+from src.audio import findPeak, Audio
 
-# Set Plot Range [-RANGE,RANGE], default is nyquist/2
-RANGE = None
-if not RANGE:
-    RANGE = RATE/2
-
-# Set these parameters (How much data to plot per FFT)
-INPUT_BLOCK_TIME = 0.05
-INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
-
-# Which Channel? (L or R)
-LR = "l"
-
-def gaussian(x, x0, y0, k):
-    return -k*(x-x0)**2+y0
 
 class SpectrumAnalyzer():
     def __init__(self):
-        self.pa = pyaudio.PyAudio()
-        self.initMicrophone()
+        self.audio=Audio()
         self.initUI()
 
-    def find_input_device(self):
-        device_index = None
-        for i in range(self.pa.get_device_count()):
-            devinfo = self.pa.get_device_info_by_index(i)
-            if devinfo["name"].lower() in ["mic","input"]:
-                device_index = i
-
-        return device_index
-
-    def initMicrophone(self):
-        device_index = self.find_input_device()
-
-        self.stream = self.pa.open(	format = FORMAT,
-                                       channels = CHANNELS,
-                                       rate = RATE,
-                                       input = True,
-                                       input_device_index = device_index,
-                                       frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
-
-    def readData(self):
-        block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
-        count = len(block)/2
-        format = "%dh"%(count)
-        shorts = struct.unpack( format, block )
-        if CHANNELS == 1:
-            return np.array(shorts)
-        else:
-            l = shorts[::2]
-            r = shorts[1::2]
-            if LR == 'l':
-                return np.array(l)
-            else:
-                return np.array(r)
 
     def initUI(self):
         self.app = QtGui.QApplication([])
@@ -98,8 +30,8 @@ class SpectrumAnalyzer():
         self.fitItem = self.specWid.getPlotItem()
         self.specItem = self.specWid.getPlotItem()
         self.specItem.setMouseEnabled(y=False)
-        self.specItem.setYRange(0,1000)
-        self.specItem.setXRange(1000, 2000, padding=0)
+        self.specItem.setYRange(0,2000)
+        self.specItem.setXRange(60, 10000, padding=0)
 
         self.specAxis = self.specItem.getAxis("bottom")
         self.specAxis.setLabel("Frequency [Hz]")
@@ -109,48 +41,23 @@ class SpectrumAnalyzer():
         self.app.aboutToQuit.connect(self.close)
 
     def close(self):
-        self.stream.close()
+        self.audio.stream.close()
         sys.exit()
-
-    def get_spectrum(self, data):
-        T = 1.0/RATE
-        N = data.shape[0]
-        Pxx = (1./N)*np.fft.fft(data)
-        f = np.fft.fftfreq(N,T)
-        Pxx = np.fft.fftshift(Pxx)
-        f = np.fft.fftshift(f)
-
-        Pxx = Pxx[len(Pxx)//2:]
-        f = f[len(f)//2:]
-
-        return f, (np.absolute(Pxx))
 
     def mainLoop(self):
         while 1:
             # Sometimes Input overflowed because of mouse events, ignore this
             try:
-                data = self.readData()
+                data = self.audio.readData()
             except IOError:
                 continue
-            f, Pxx = self.get_spectrum(data)
-
-            i_max = Pxx.argmax(axis=0)
-
-            if i_max > 5:
-                try:
-                    fR = f[i_max-1:i_max+2]
-                    PR = Pxx[i_max-1:i_max+2]
-                    popt, _ = optimize.curve_fit(gaussian, fR, PR)
-                    x2 = np.linspace(fR[0], fR[-1], 10)
-                    y2 = gaussian(x2, *popt)
-
-                    M = y2.argmax(axis=0)
-                    print(x2[M], f[i_max])
-                    self.fitItem.plot(x=x2,y=y2, clear=True, pen=mkPen('r', width=3))
-                except Exception:
-                    pass
-
-            self.specItem.plot(x=f,y=Pxx, clear=False)
+            f, Pxx = self.audio.get_spectrum(data)
+            info=findPeak(f,Pxx)
+            if info is not None:
+                self.fitItem.plot(x=info[1], y=info[2], clear=True, pen=mkPen('r', width=3))
+                self.specItem.plot(x=f,y=Pxx, clear=False)
+            else:
+                self.specItem.plot(x=f, y=Pxx, clear=True)
             QtGui.QApplication.processEvents()
 
 if __name__ == '__main__':
